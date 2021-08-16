4192e39bf65b1f61e9e2399625c92eca94c10355
Yao M <my163cih@gmail.com>
2021-08-16 17:43:38 -0400

    connected to DB, added sample data, api routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import products from "./data/products.js";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,13 +17,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on ${PORT}`));
